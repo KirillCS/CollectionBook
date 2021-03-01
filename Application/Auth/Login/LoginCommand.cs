e81e2f6b208b78b28a90a1c7c07474c31bab1df7
KirillCS <KirillCS@users.noreply.github.com>
2021-03-01 12:55:19 +0300

    Login command was added

---
+++ Application/Auth/Login/LoginCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Auth.Login
+{
+    public class LoginCommand : IRequest<string>
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+    {
+        public LoginCommandHandler()
+        {
+
+        }
+
+        public Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
