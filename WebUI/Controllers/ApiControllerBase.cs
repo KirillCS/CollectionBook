a5dafbfd2145f91a9037ede9033faeb21ef93a3d
KirillCS <KirillCS@users.noreply.github.com>
2021-03-01 12:46:16 +0300

    Auth controller was added

---
+++ WebUI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+        private ISender mediator;
+
+        public ISender Mediator => mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
