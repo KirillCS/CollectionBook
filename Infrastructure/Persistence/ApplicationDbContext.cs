8cd141644f5231be1dc085dd1d78933b2f45154e
KirillCS <KirillCS@users.noreply.github.com>
2021-03-01 12:37:08 +0300

    Application context was added

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public async Task<int> SaveChanges(CancellationToken cancellationToken)
+        {
+            return await SaveChangesAsync(cancellationToken);
+        }
+    }
+}
