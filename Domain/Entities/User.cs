528d39e7ddf0fb9229dc66da496014c3f59947e0
KirillCS <KirillCS@users.noreply.github.com>
2021-03-09 11:33:15 +0300

    User properties was added

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -4,6 +4,20 @@ namespace Domain.Entities
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Location { get; set; }
+
+        public string Biography { get; set; }
+
+        public string TelegramLogin { get; set; }
+
+        public string InstagramLogin { get; set; }
+
+        public bool IsEmailVisible { get; set; }
+
         public User()
         { }
 
---
+++ Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(128);
+
+            builder.Property(u => u.LastName).HasMaxLength(128);
+
+            builder.Property(u => u.Location).HasMaxLength(256);
+
+            builder.Property(u => u.IsEmailVisible).HasDefaultValue(false);
+        }
+    }
+}
