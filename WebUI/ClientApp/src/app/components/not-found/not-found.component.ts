bd28d6e71a7e20d57a41d6d766fec054f5eb8c97
KirillCS <KirillCS@users.noreply.github.com>
2021-03-10 17:36:54 +0300

    CB-4: go home and go back buttons was added

--- WebUI/ClientApp/src/app/components/not-found/not-found.component.ts
+++ WebUI/ClientApp/src/app/components/not-found/not-found.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
+export class NotFoundComponent {
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
   }
 
+  public goBack() {
+    this.location.back();
+  }
 }
