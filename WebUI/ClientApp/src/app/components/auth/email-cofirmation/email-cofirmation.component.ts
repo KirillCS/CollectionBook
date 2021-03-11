5656fb7b426731fac563c6c9b5d83d4a17be19a8
KirillCS <KirillCS@users.noreply.github.com>
2021-03-11 16:04:36 +0300

    CB-5: email confirmation page was added

---
+++ WebUI/ClientApp/src/app/components/auth/email-cofirmation/email-cofirmation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-cofirmation',
+  templateUrl: './email-cofirmation.component.html',
+  styleUrls: ['./email-cofirmation.component.scss']
+})
+export class EmailCofirmationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
