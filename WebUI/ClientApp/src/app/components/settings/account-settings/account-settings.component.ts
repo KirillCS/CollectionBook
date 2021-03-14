6948eebdb43cfebbb77ecd7e184bbd391657db94
KirillCS <KirillCS@users.noreply.github.com>
2021-03-14 22:14:47 +0300

    CB-6: settings page was made up

--- WebUI/ClientApp/src/app/components/settings/account-settings/account-settings.component.ts
+++ WebUI/ClientApp/src/app/components/settings/account-settings/account-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { SubmitErrorStateMatcher } from 'src/app/error-state-matchers/submit-error-state-matcher'
 
 @Component({
   selector: 'app-account-settings',
@@ -6,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
+  public matcher = new SubmitErrorStateMatcher();
+  public form = new FormGroup({ 
+    login: new FormControl(),
+    email: new FormControl('', Validators.email)
+  });
+
+  public inProcess = false;
+  
+  public get login() : AbstractControl {
+    return this.form.get('login');
+  }
+  
+  public get email() : AbstractControl {
+    return this.form.get('email');
+  }
 
   constructor() { }
 
