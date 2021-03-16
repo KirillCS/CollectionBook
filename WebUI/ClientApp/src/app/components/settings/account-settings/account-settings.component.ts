f36a78868ed243b3a7fdcf6d69d44c92f423e5ef
KirillCS <KirillCS@users.noreply.github.com>
2021-03-16 20:04:41 +0300

    CB-6: changing login error handling was add

--- WebUI/ClientApp/src/app/components/settings/account-settings/account-settings.component.ts
+++ WebUI/ClientApp/src/app/components/settings/account-settings/account-settings.component.ts
@@ -11,6 +11,9 @@ import { CurrentUserService } from 'src/app/services/current-user.service';
 import { SettingsService } from 'src/app/services/settings.service';
 import { UserService } from 'src/app/services/user.service';
 import { DialogComponent } from 'src/app/components/dialogs/dialog/dialog.component';
+import { ServerErrorsService } from 'src/app/services/server-errors.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { MessageDialogComponent, MessageDialogData, MessageDialogType } from 'src/app/components/dialogs/message-dialog/message-dialog.component';
 
 @Component({
   selector: 'app-account-settings',
@@ -27,13 +30,16 @@ export class AccountSettingsComponent implements OnInit, OnDestroy {
 
   public user: UserDto;
 
-  public inProcess = false;
+  public isChangingLoginInProcess = false;
+  public isChangingEmailInProcess = false;
 
   constructor(
     private settingsService: SettingsService,
     private userService: UserService,
     private currentUserService: CurrentUserService,
     private authTokenService: AuthTokenService,
+    private serverErrorService: ServerErrorsService,
+    private authService: AuthService,
     private dialog: MatDialog
   ) {
     this.subscription = settingsService.user$.subscribe(user => {
@@ -44,7 +50,24 @@ export class AccountSettingsComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userService.getUser(this.currentUserService.currentUser.login).subscribe(user => {
       this.settingsService.update(user);
-    })
+    }, (errorResponse: HttpErrorResponse) => {
+      if (errorResponse.status == 404) {
+        this.authService.logout();
+        this.dialog.open(MessageDialogComponent, {
+          width: '400px',
+          position: { top: '30vh' },
+          data: {
+            header: 'Not found',
+            message: `Your account was not found. Maybe it was deleted.`,
+            buttonName: 'OK'
+          }
+        });
+
+        return;
+      }
+
+      this.dialog.open(MessageDialogComponent, { width: '400px', position: { top: '30vh' }});
+    });
   }
 
   ngOnDestroy(): void {
@@ -79,15 +102,55 @@ export class AccountSettingsComponent implements OnInit, OnDestroy {
         return;
       }
 
+      this.isChangingLoginInProcess = true;
       this.userService.updateLogin({ login: control.value }).subscribe(response => {
         this.authTokenService.setToken(response.accessToken);
         this.user.login = control.value;
         this.settingsService.update(this.user);
 
       }, (errorResponse: HttpErrorResponse) => {
+        if (errorResponse.status == 400) {
+          this.serverErrorService.setFormErrors(this.loginForm, errorResponse);
+          this.isChangingLoginInProcess = false;
 
+          return;
+        }
+
+        if (errorResponse.status == 401) {
+          this.authService.logout();
+          this.dialog.open(MessageDialogComponent, {
+            width: '400px',
+            position: { top: '30vh' },
+            data: {
+              header: 'Not authenticated',
+              message: `You must be authenticated to change account login.`,
+              buttonName: 'OK'
+            }
+          });
+          
+          return;
+        }
+        
+        if (errorResponse.status == 404) {
+          this.authService.logout();
+          this.dialog.open(MessageDialogComponent, {
+            width: '400px',
+            position: { top: '30vh' },
+            data: {
+              header: 'Not found',
+              message: `Your account was not found. Maybe it was deleted.`,
+              buttonName: 'OK'
+            }
+          });
+
+          return;
+        }
+
+        this.dialog.open(MessageDialogComponent, { width: '400px', position: { top: '30vh' }});
+        this.isChangingLoginInProcess = false;
       }, () => {
         form.resetForm();
+        this.isChangingLoginInProcess = false;
       });
     });
   }
