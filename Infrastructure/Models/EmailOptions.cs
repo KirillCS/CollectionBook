b050af96b0beb0809f937f6eba1a418d93b128ef
KirillCS <KirillCS@users.noreply.github.com>
2021-03-17 10:26:11 +0300

    CB-6: emails content was replaced to appsettings

--- Infrastructure/Models/EmailOptions.cs
+++ Infrastructure/Models/EmailOptions.cs
@@ -5,5 +5,13 @@
         public string Address { get; set; }
 
         public string Name { get; set; }
+
+        public string ConfirmationSubject { get; set; }
+
+        public string ConfirmationMessage { get; set; }
+
+        public string ChangingSubject { get; set; }
+
+        public string ChangingMessage { get; set; }
     }
 }
