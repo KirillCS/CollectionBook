5b9d1c4119434fbae069e0ca15a61859d092af72
KirillCS <KirillCS@users.noreply.github.com>
2021-03-18 12:25:57 +0300

    CB-7: user was added to infrastructure

---
+++ Infrastructure/Identity/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Location { get; set; }
+
+        public string Biography { get; set; }
+
+        public string WebsiteUrl { get; set; }
+
+        public string TelegramLogin { get; set; }
+
+        public string InstagramLogin { get; set; }
+
+        public bool IsEmailVisible { get; set; }
+
+        public string AvatarPath { get; set; }
+
+        public User()
+        { }
+
+        public User(string userName) : base(userName)
+        { }
+    }
+}
