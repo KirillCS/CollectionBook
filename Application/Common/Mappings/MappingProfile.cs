5c3e2c34c59b9ea2ca6ce128bc4c565af0883cb2
KirillCS <KirillCS@users.noreply.github.com>
2021-03-18 12:21:47 +0300

    CB-7: IMapTo was added

--- Application/Common/Mappings/MappingProfile.cs
+++ Application/Common/Mappings/MappingProfile.cs
@@ -10,6 +10,7 @@ namespace Application.Common.Mappings
         public MappingProfile()
         {
             ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+            ApplyMappingsToAssembly(Assembly.GetExecutingAssembly());
         }
 
         private void ApplyMappingsFromAssembly(Assembly assembly)
@@ -22,6 +23,20 @@ namespace Application.Common.Mappings
             {
                 var instance = Activator.CreateInstance(type);
                 var methodInfo = type.GetMethod("Mapping") ?? type.GetInterface("IMapFrom`1").GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+
+        private void ApplyMappingsToAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                                .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapTo<>)))
+                                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping") ?? type.GetInterface("IMapTo`1").GetMethod("Mapping");
                 methodInfo?.Invoke(instance, new object[] { this });
             }
         }
