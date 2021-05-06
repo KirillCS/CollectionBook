f0581e7536bc1c603a2647c3472ae41689912393
KirillCS <KirillCS@users.noreply.github.com>
2021-05-06 21:35:46 +0300

    CB-17: displaying item was added

--- WebUI/ClientApp/src/app/components/item/item.component.ts
+++ WebUI/ClientApp/src/app/components/item/item.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { API_URL, DEFAULT_COLLECTION_COVER } from 'src/app/app-injection-tokens';
+import { ItemDto } from 'src/app/models/dtos/item/item.dto';
 
 import { UserCoverDto } from 'src/app/models/dtos/user/user-cover.dto';
+import { ItemService } from 'src/app/services/item.service';
 import { PathNode } from '../ui/path/path-node';
 
 @Component({
@@ -10,23 +14,43 @@ import { PathNode } from '../ui/path/path-node';
 })
 export class ItemComponent implements OnInit {
 
-  public user: UserCoverDto = {
-    id: null,
-    login: 'login',
-    firstName: 'Kirill',
-    lastName: 'Potapov',
-    avatarPath: null
+  private _item: ItemDto;
+  private _pathNodes: Array<PathNode>;
+
+  public constructor(
+    @Inject(API_URL) private apiUrl: string,
+    @Inject(DEFAULT_COLLECTION_COVER) private defaul: string,
+    private itemService: ItemService,
+    private route: ActivatedRoute
+  ) { }
+
+  public get pathNodes() : Array<PathNode> {
+    return this._pathNodes;
+  }
+  
+  public get item() : ItemDto {
+    return this._item;
   }
 
-  public pathNodes: Array<PathNode> = [
-    new PathNode('login', '/profile/login'),
-    new PathNode('Books', '/collection/22'),
-    new PathNode('Some item')
-  ]
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public get defaultImagePath() : string {
+    return this.defaul;
+  }
+  
+  public ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let id = parseInt(params.get('id'));
+      this.itemService.get(id).subscribe(item => {
+        this._item = item;
+        this._pathNodes = [
+          new PathNode(item.user.login, `/profile/${item.user.login}`),
+          new PathNode(item.collection.name, `/collection/${item.collection.id}`),
+          new PathNode(item.name)
+        ]
+      });
+    });
   }
 
+  public getFullImagePath(imagePath: string): string {
+    return `${this.apiUrl}${imagePath}`
+  }
 }
