b422352d032c99050fd97b85e87649bd449fc119
KirillCS <KirillCS@users.noreply.github.com>
2021-05-30 14:01:41 +0300

    CB-20: roles seed was added

---
+++ Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!await roleManager.RoleExistsAsync(Roles.Owner))
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.Owner));
+            }
+
+            if (!await roleManager.RoleExistsAsync(Roles.Admin))
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+            }
+        }
+    }
+}
--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,14 +1,35 @@
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace WebUI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                ApplicationDbContext context = services.GetRequiredService<ApplicationDbContext>();
+                    
+                context.Database.Migrate();
+
+                RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                await ApplicationDbContextSeed.SeedRoles(roleManager);
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
