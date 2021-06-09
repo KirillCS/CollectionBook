a95e45225a21f3963b748385fa0fcaba44830e83
KirillCS <KirillCS@users.noreply.github.com>
2021-06-09 22:55:57 +0300

    AuthController was refactored

--- WebUI/Controllers/AuthController.cs
+++ WebUI/Controllers/AuthController.cs
@@ -9,16 +9,12 @@ namespace WebUI.Controllers
     {
         [Route("login")]
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
+        public async Task<IActionResult> Login([FromBody] LoginCommand command) =>
+            Ok(await Mediator.Send(command));
 
         [Route("register")]
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody] RegisterCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
+        public async Task<IActionResult> Register([FromBody] RegisterCommand command) =>
+            Ok(await Mediator.Send(command));
     }
 }
